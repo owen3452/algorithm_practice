93105c3c56c665dcd6c4bc39e57584195bebbbbb
zhendingcheng <Z1d2c3xin!!>
2018-12-25 23:06:18 +0800

    无

--- naive_bayes.py
+++ naive_bayes.py
@@ -16,16 +16,16 @@ class naive_bayes(object):
         self._distribution = distribution
 
     def handle_gaussian(self, X, x):
-        # 很多博客反馈scipy的fit不准，待改进
         u, sigma = stats.norm.fit(X)
         return stats.norm.pdf(self, x, u, sigma)
 
     def handle_exponential(self, X, x):
-        # 很多博客反馈scipy的fit不准，待改进
+        # 很多博客反馈stats.expon使用最大似然拟合的效果不好
         loc, scale = stats.expon.fit(X)
         return stats.expon(self, x, loc, scale)
 
     def fit(self, X, y):
+
 
 
 
