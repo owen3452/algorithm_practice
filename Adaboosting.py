4ccd921e0c3cee0858597cc1a5b0a937e38c0c26
zhendingcheng <Z1d2c3xin!!>
2019-01-13 23:09:49 +0800

    adaboosting

--- Adaboosting.py
+++ Adaboosting.py
@@ -30,8 +30,21 @@ class AdaboostingClassifier(object):
         self._min_impurity_decrease = min_impurity_decrease
         self._min_impurity_split = min_impurity_split
 
+    def handle_weight(self, X, y, error_list):
+        '''
+        由于DecisionTree未加入weight，所以adaboosting中将预测错样本weight*e^2（约7）更新权重
+        '''
+        for x_i, i in zip(X, error_list):
+            if i==1:
+                np.row_stack((X, x_i, x_i, x_i, x_i, x_i, x_i, x_i))
+        for y_i, i in zip(y, error_list):
+            if i==1:
+                np.append(y, [y_i, y_i, y_i, y_i, y_i, y_i, y_i])
+        return X, y
+
     def fit(self, X, y):
-        m, n = np.shape(X)
+        self.model_list = np.array([])
+        self.alpha_list = np.array([])
         for i in range(self._n_estimators):
             tree_tmp = DecisionTree(type=self._type, criterion=self._criterion, splitter=self._splitter,
                                     min_impurity_decrease=self._min_impurity_decrease,
@@ -39,3 +52,18 @@ class AdaboostingClassifier(object):
                                     min_samples_split=self._min_samples_split, max_depth=self._max_depth)
             tree_tmp.fit(X, y)
             predict_tmp = tree_tmp.predict(X)
+            error_list = [1 if a != b else 0 for a, b in zip(y, predict_tmp)]
+            error_rate = sum(error_list) * 1.0 / len(error_list)
+            alpha = 0.5 * np.exp(1 / error_rate - 1)
+            self.model_list.append(tree_tmp)
+            self.alpha_list.append(alpha)
+            X, y = self.handle_weight(self, X, y, error_list)
+
+    def predict_prob(self, X):
+        result = []
+        for x in X:
+            predict_list = np.array([model.predict(x) for model in self.model_list])
+            result.append(np.dot(predict_list, self.alpha_list.T))
+        return np.array(result)
+
+
